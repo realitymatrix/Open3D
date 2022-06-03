bc0110e416b9eca34e9eb3bb15d8f61d87d39bbc
Petr Korolev <peterkorolev95@gmail.com>
2022-06-03 12:25:56 +0300

    target cpu

--- 3rdparty/webrtc/webrtc_common.cmake
+++ 3rdparty/webrtc/webrtc_common.cmake
@@ -11,7 +11,7 @@ function(get_webrtc_args WEBRTC_ARGS)
 
     # System Architecture ARM64 on Linux
     set(WEBRTC_ARGS target_os=linux\n${WEBRTC_ARGS})
-    set(WEBRTC_ARGS target_cpu=arm\n${WEBRTC_ARGS})
+    set(WEBRTC_ARGS target_cpu=arm64\n${WEBRTC_ARGS})
 
     if(NOT MSVC)
         # ABI selection
