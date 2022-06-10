31a0655320f2c50708dc6f0e6baa10a9a3503b70
Petr Korolev <peterkorolev95@gmail.com>
2022-06-10 18:07:49 +0300

    no platform flag for docker

--- 3rdparty/webrtc/docker_build_webrtc.sh
+++ 3rdparty/webrtc/docker_build_webrtc.sh
@@ -1,1 +1,1 @@
-sudo docker buildx build --platform linux/arm64 -t open3d-webrtc:abi1 -f ./Open3D/3rdparty/webrtc/Dockerfile.webrtc .
+sudo docker build -t open3d-webrtc:abi1 -f ./Open3D/3rdparty/webrtc/Dockerfile.webrtc .
