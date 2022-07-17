3dd40a0991f175cad3902674789a73229f1a5e90
Petr Korolev <peterkorolev95@gmail.com>
2022-07-17 12:53:52 +0300

    debug

--- 3rdparty/webrtc/webrtc_common.cmake
+++ 3rdparty/webrtc/webrtc_common.cmake
@@ -18,7 +18,7 @@ function(get_webrtc_args WEBRTC_ARGS)
     set(WEBRTC_ARGS use_lld=false\n${WEBRTC_ARGS})
     set(WEBRTC_ARGS is_component_build=false\n${WEBRTC_ARGS})
     set(WEBRTC_ARGS use_cxx11=true\n${WEBRTC_ARGS})
-#    set(WEBRTC_ARGS use_remoteexec=true\n${WEBRTC_ARGS})
+    set(WEBRTC_ARGS use_remoteexec=false\n${WEBRTC_ARGS})
     
 
 #    if(NOT MSVC)
